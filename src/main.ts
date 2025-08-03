544b636f094f91db11109ac1ca732ace2db36e7d
TienAnhh <buitienanhngu@gmail.com>
2025-08-04 00:32:37 +0700

    Second commit

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
 import './styles/style.css';
 
 // Import main JavaScript
-import './js/main.js';
+import '../js/main.js';
 
 // Initialize the app
 document.addEventListener('DOMContentLoaded', () => {
